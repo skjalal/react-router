3477205ef6f0ab0c590c2b22b39d1e734a5dc492
Jalaluddin Shaikh <jalaluddin.shaikh@sunware.in>
2025-12-30 18:15:07 +0530

    added second page

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 import {
   createBrowserRouter,
   RouterProvider,
+  type DataRouter,
   type RouteObject,
 } from "react-router-dom";
 
 import HomePage from "./pages/Home.tsx";
+import ProductsPage from "./pages/Product.tsx";
 import "./App.css";
 
-const routes: RouteObject[] = [{ path: "/", element: <HomePage /> }];
+const routes: RouteObject[] = [
+  { path: "/", element: <HomePage /> },
+  { path: "/products", element: <ProductsPage /> },
+];
 
-const router = createBrowserRouter(routes);
+const router: DataRouter = createBrowserRouter(routes);
 
 function App() {
   return <RouterProvider router={router} />;
---
+++ src/pages/Product.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ProductsPage: React.FC = () => {
+  return <h1>This is Products Page</h1>;
+};
+
+export default ProductsPage;
