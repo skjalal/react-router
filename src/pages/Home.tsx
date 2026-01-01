3399abea2fb69b06f4d6c634906b963bec5ca60d
Jalaluddin Shaikh <jalaluddin.shaikh@sunware.in>
2026-01-01 14:58:38 +0530

    Navigate programmatically

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,21 @@
 import React, { type JSX } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const HomePage: React.FC = (): JSX.Element => {
+  const navigate = useNavigate();
+
+  const navigateHandler = (): void => {
+    navigate("/products");
+  };
+
   return (
     <>
       <h1>My Home Page</h1>
       <p>
         <Link to="/products">Go to Product Page</Link>
+      </p>
+      <p>
+        <button onClick={navigateHandler}>Navigate to Product Page</button>
       </p>
     </>
   );
