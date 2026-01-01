ebdf8c4fd81a72a923940484f199855af5db8cd4
Jalaluddin Shaikh <jalaluddin.shaikh@sunware.in>
2026-01-01 14:11:48 +0530

    alternative approach of route declaration

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import {
   createBrowserRouter,
+  //createRoutesFromElements,
   RouterProvider,
+  //Route,
   type DataRouter,
   type RouteObject,
 } from "react-router-dom";
@@ -9,10 +11,19 @@ import HomePage from "./pages/Home.tsx";
 import ProductsPage from "./pages/Product.tsx";
 import "./App.css";
 
+//Object based approach
 const routes: RouteObject[] = [
   { path: "/", element: <HomePage /> },
   { path: "/products", element: <ProductsPage /> },
 ];
+
+//Element based approach
+// const routes: RouteObject[] = createRoutesFromElements(
+//   <Route>
+//     <Route path="/" element={<HomePage />} />
+//     <Route path="/products" element={<ProductsPage />} />
+//   </Route>
+// );
 
 const router: DataRouter = createBrowserRouter(routes);
 
