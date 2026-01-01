8d58e5f9d4c13eb03580c7a897c6c21401ba20ec
Jalaluddin Shaikh <jalaluddin.shaikh@sunware.in>
2026-01-01 14:15:44 +0530

    Added navigate using Link

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,15 @@
 import React, { type JSX } from "react";
+import { Link } from "react-router-dom";
 
 const HomePage: React.FC = (): JSX.Element => {
-  return <h1>My Home Page</h1>;
+  return (
+    <>
+      <h1>My Home Page</h1>
+      <p>
+        <Link to="/products">Go to Product Page</Link>
+      </p>
+    </>
+  );
 };
 
 export default HomePage;
